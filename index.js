75a2dc43a164275aeeaff913cf28136c762e5130
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2023-09-07 21:06:08 +0530

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 
-app.use('/add-product', (req, res, next) => {
+app.use('/add-product', (req, res,) => {
    res.send(`
    <form action="/product" method="POST">
        <input type="text" name="productName" placeholder="Product Name">
@@ -19,16 +19,16 @@ app.use('/add-product', (req, res, next) => {
 `)
 });
 
-app.use('/product', (req, res, next) => {
+app.use('/product', (req, res,) => {
     console.log(req.body);
     res.redirect('/success')
 });
-app.use('/success', (req, res, next) => {
+app.use('/success', (req, res, ) => {
     
     res.send('<h1>Added successefully</h1>')
 });
 
-app.use('/', (req, res, next) => {
+app.use('/', (req, res,) => {
    
     res.send('<h1>Hello from Express</h1>')
 });
